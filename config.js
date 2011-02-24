77575b3ac2e062013a13d756211dc5ccb10e2e99
mkrecny <mkrecny@gmail.com>
2011-02-24 15:25:39 -0500

    generalizing the config file :p

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //The websocket server
-exports.server_uri = '33.33.33.15';
+exports.server_uri = 'localhost';
 exports.server_port = 8080;
 
 // Redis - if assign null, defaults to localhost and port 6379
